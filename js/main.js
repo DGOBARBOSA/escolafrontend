fa8fee68ae1c79f4bea037ea34b18e385acc96bc
Diogo Barbosa <leitholdi@yahoo.com.br>
2019-07-01 14:35:12 -0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slideToShow: 1,
+    slideToScroll: 6,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+
+});
